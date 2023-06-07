c304fd1b39faeb97798450f921fa50ee1e01ebaa
Commit 7/6 <duy241102@gmail.com>
2023-06-07 09:52:19 +0700

    Commit 7/6

---
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,98 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Book;
+import com.example.demo.services.BookService;
+import com.example.demo.services.CategoryService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/book")
+public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    @Autowired
+    private CategoryService categoryService;
+    @GetMapping
+    public String showAllBook(Model model) {
+
+        model.addAttribute("books", bookService.getAllBooks());
+        model.addAttribute("title", "Book List");
+        return "book/list";
+    }
+
+    @GetMapping("/add")
+    public String addBookForm(Model model) {
+
+        model.addAttribute("book", new Book());
+        model.addAttribute("categories", categoryService.getAllCategories());
+        return "book/add";
+    }
+
+/*    @PostMapping("/add")
+    public String addBook(@ModelAttribute("book") Book book) {
+        bookService.addBook(book);
+        return "redirect:/books";
+    }*/
+
+    @PostMapping("/add")
+    public String addBook(@Valid @ModelAttribute("book") Book book, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            //model.addAttribute("book", new Book());
+            model.addAttribute("categories", categoryService.getAllCategories());
+            return "book/add";
+        }
+        bookService.addBook(book);
+        return "redirect:/book";
+    }
+
+
+    @GetMapping("/edit/{id}")
+    public String editBookForm(@PathVariable("id") long id, Model model){
+        Book editBook = bookService.getBookById(id);
+        if(editBook != null){
+            model.addAttribute("book", editBook);
+            model.addAttribute("categories", categoryService.getAllCategories());
+            return "book/edit";
+        }else {
+            return "not-found";
+        }
+    }
+    @PostMapping("/edit")
+    public String editBook(@Valid @ModelAttribute("book")Book updateBook, BindingResult bindingResult, Model model ){
+        if (bindingResult.hasErrors()){
+            model.addAttribute("categories", categoryService.getAllCategories());
+            return "book/edit";
+        }
+        bookService.getAllBooks().stream()
+                .filter(book -> book.getId() == updateBook.getId())
+                .findFirst()
+                .ifPresent(book -> {
+                    bookService.updateBook(updateBook);
+                });
+        return "redirect:/book";
+    }
+    @PostMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") long id){
+        bookService.deleteBook(id);
+        return "redirect:/book";
+    }
+
+    @GetMapping ("/delete/{id}")
+    public String deleteBook (@PathVariable ("id") Long id)  {
+
+        bookService.deleteBook(id);
+        return "redirect:/book";
+    }
+
+}
